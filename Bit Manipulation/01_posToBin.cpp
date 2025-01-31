a19281223b612690662ef49599cc3ba71280dd2c
srivastavHimanshu4503 <codeforinterviewwithhimanshu@gmail.com>
2025-01-31 16:55:45 +0530

    Binary Converter using bitwise operations

--- Bit Manipulation/01_posToBin.cpp
+++ Bit Manipulation/01_posToBin.cpp
@@ -1,29 +1,39 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
+
+void usingDivisonMethod(int posNum) {    
+    int binaryNum = 0;
+    int i = 0;
+    while(posNum) {
+        int bit = posNum%2;
+        binaryNum += bit*pow(10, i++);
+        posNum  /= 2;
+    }
+
+    cout << "Binary form of given positive number: " << binaryNum << endl;
+}
+
+void usingBitwiseMethod(int posNum) {    
+    int binaryNum = 0;
+    int i = 0;
+    while(posNum) {
+        int bit = posNum & 1;
+        binaryNum += bit*pow(10, i++);
+        posNum  >>= 1;
+    }
+
+    cout << "Binary form of given positive number: " << binaryNum << endl;
+}
 
 int main() {
 
-    int s = 0, ans = 1, binRep = 0;
     int posNum;
     cout << "Enter positive number: ";
     cin >> posNum;
-    
-    
-    while(posNum) {
-        if(s) {
-            for(int i = 0; i < 1; i++) {
-                ans *= 10;
-            }   
-        }
-        else
-            ans = 1;
-        int bit = posNum&1;
-        binRep = bit*ans + binRep;
-        s++;
-        posNum >>= 1;
-    }
-    cout << "Binary form of given positive number: " << binRep << endl;
 
-    
+    usingBitwiseMethod(posNum);
+    usingDivisonMethod(posNum);
+
     return 0;
 }
